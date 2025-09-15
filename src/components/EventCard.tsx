1691049032e1c8d1f8d0112ce58dfa99b9ced8de
Constantine Spanos <cspanos.m@gmail.com>
2025-09-14 22:38:58 -0400

    tuneup

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -32,7 +32,7 @@ export function EventCard({
       animate={controls}
       variants={variants}
       transition={{
-        duration: 0.5,              // 👈 longer fade (1s)
+        duration: 0.65,              // 👈 longer fade (1s)
         ease: "easeInOut",          // 👈 smoother easing
       }}
       className={className}
